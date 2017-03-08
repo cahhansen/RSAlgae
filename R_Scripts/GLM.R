3cd2438f1bb08e04fea9208c00098f200657484a
cahhansen <carlyahansen@gmail.com>
2017-03-07 22:54:43 -0700

    Add GLM.R

---
+++ R_Scripts/GLM.R
@@ -0,0 +1,37 @@
+
+#Load in packages
+library(ggplot2)
+library(lubridate)
+
+#Load data and process-------------------------------------------------------------------------------
+setwd("C:/Users/carly/Google Drive/University of Utah - Research/Dissertation Research/RemoteSensingPaper")
+#Load dataframe
+load('ReflectanceValues/SurfaceReflectanceData/UtahLakeLS57ModelData.Rda')
+dataframe=dataframe[which(dataframe$Dataset=="Calibration"),]
+dataframe$Month=as.factor(month(dataframe$SamplingDate))
+dataframe=dataframe[(dataframe$Month=="7"|dataframe$Month=="8"|dataframe$Month=="9"),]
+timewindow=1
+subdataframe=dataframe[which(dataframe$AbsDiffInDays<=timewindow),]
+
+modelvariables=c("Blue","Green","NIR","Red","SWIR1","SWIR2","Green_Blue","Red_Blue","Red_Green",
+                 "Red_NIR","Red_SWIR1","Green_SWIR1","Blue_SWIR1","Red_SWIR2","Green_SWIR2","Blue_SWIR2",
+                 "NIR_SWIR1","NIR_SWIR2","NIR_Blue","NIR_Green","NDVI","avgRGB","avgSWIR","FieldValue")
+moddataframe=subset(subdataframe,select=modelvariables)
+mod=glm(FieldValue~Blue+Red_SWIR2+Green_SWIR1,data=moddataframe,family=gaussian(link="log"))
+predicted=(mod$fitted.values)
+compare=data.frame(predicted)
+compare$actual=moddataframe$FieldValue
+compare$diff=compare$actual-compare$predicted
+#Compute RMSE
+rmse=sqrt(mean(compare$diff)^2)
+print(paste("RMSE is: ",rmse))
+summary(lm(compare$actual~compare$predicted)$r.squared)
+print(summary(lm(compare$actual~compare$predicted))$r.squared)
+summary(mod)
+
+print(paste("Min chl: ",min(moddataframe$FieldValue)," Max chl: ",max(moddataframe$FieldValue)))
+print(paste("N:",nrow(moddataframe)))
+ggplot()+geom_point(aes(x=subdataframe$FieldValue,y=compare$predicted))+
+  xlab(expression(paste("Measured Chl (",mu,"g/L)")))+ylab(expression(paste("Modeled Chl (",mu,"g/L)")))+
+  theme_bw()+ggtitle("Measured vs. Modeled Chlorophyll, Late Summer Model")+
+  theme(plot.title = element_text(hjust = 0.5))+geom_abline()
