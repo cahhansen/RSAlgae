204ac609c87a4449da8fde0430b7e639e54c3f45
cahhansen <carlyahansen@gmail.com>
2017-10-11 16:09:18 -0600

    update climate.factor.effect function

--- RSAlgaeR/R/FactorAnalysisFunctions.R
+++ RSAlgaeR/R/FactorAnalysisFunctions.R
@@ -25,167 +25,99 @@ lagpad <- function(x, k) {
 
 }
 
-
-#' climate.factor.compare
+#' climate.factor.effect
 #'
-#' Performs a t-test on water quality dataset for various climate variables
-#'
-#' @param record dataframe with estimated historical record of water quality
-#' @param value string, name of column with estimated or field-sampled water quality parameter
-#' @param climaterecord dataframe with climate variables
-#' @param climatevar character, name of climate variable (column) of interest
-#' @param lag integer, number of days to lag the climate variable
-#' @param noevent numeric, threshold for whether an event occured
-#' @param alternative character string specifying alternative hypothesis ("two.sided","greater","less")
-#' @param months an optional character string for if the t-test should be month specific
-#' @import stats
-#' @import lubridate
-#' @export
-
-
-climate.factor.compare <- function(record,value,climaterecord,climatevar,lag=NULL,noevent,alternative="two.sided",months=NULL){
-  record$value <- record[,value]
-  #If the variable is lagged:
-  if(!is.null(lag)){
-    #Lag climatevar
-    laggedvar <- lagpad(climaterecord[,climatevar],lag)
-    climaterecord[,paste(climatevar,'_lag')] <- laggedvar
-    #Subset into groups based on climate/weather events
-    climateeventdates <- climaterecord[laggedvar>noevent,'Date']
-    record.events <- subset(record,ImageDate %in% climateeventdates)
-    record.noevents <- subset(record,!(ImageDate %in% climateeventdates))
-  }else{
-    #If the variable is not lagged:
-    #Subset into groups based on climate/weather events
-    climateeventdates <- climaterecord[(climaterecord[,climatevar]>noevent),'Date']
-    record.events <- subset(record,ImageDate %in% climateeventdates)
-    record.noevents <- subset(record,!(ImageDate %in% climateeventdates))
-  }
-
-  #If the analysis should be done for a specific month:
-  if(!is.null(months)){
-    print(months)
-    record.events$Month <- months(record.events$ImageDate)
-    record.noevents$Month <- months(record.noevents$ImageDate)
-    record.events.sub <- subset(record.events, Month %in% c(months))
-    record.noevents.sub <- subset(record.noevents, Month %in% c(months))
-    print(paste("Number Events:",nrow(record.events.sub),",Number non-Events:",nrow(record.noevents.sub)))
-    #Wilcox Test (Does not assume normal distributions)
-    results <- tryCatch(wilcox.test(record.events.sub$value,record.noevents.sub$value,alternative), error=function(e) NULL)
-
-    print(paste("Mean Events:",mean(record.events.sub$value)))
-    print(paste("Mean non-Events:", mean(record.noevents.sub$value)))
-
-    if(nrow(record.noevents.sub)>0 & nrow(record.events.sub)>0){
-      record.events.sub$Event <- 'Event'
-      record.noevents.sub$Event <- 'No Event'
-      recordcompare <- rbind(record.events.sub,record.noevents.sub)
-      #Box and whisker plot of the two comparison groups
-      p <- ggplot(recordcompare)+
-        geom_boxplot(aes(x=as.factor(Event),y=value))+
-        ggtitle(paste("Comparison of Chl Values for Climate Events: ",months))+
-        ylab(expression(paste("Chl-a ",mu,"g/L")))+
-        xlab("")+
-        theme_bw()+
-        annotate("text",x="Event",y=-8,label=paste("Mean:",round(mean(record.events.sub$value),2)))+
-        annotate("text",x="No Event",y=-8,label=paste("Mean:",round(mean(record.noevents.sub$value),2)))
-    }else{
-      recordcompare <- rbind(record.events.sub,record.noevents.sub)
-      #Box and whisker plot of all of the data together
-      p <- ggplot(recordcompare)+
-        geom_boxplot(aes(x=Lake,y=value))+
-        ggtitle(paste("Distribution of Chl Values for: ",months))+
-        ylab(expression(paste("Chl-a ",mu,"g/L")))+
-        xlab("")+
-        theme_bw()+
-        annotate("text",x=unique(recordcompare$Lake),y=-8,label=paste("Mean:",round(mean(recordcompare$value),2)))
-    }
-
-  }else{
-    #Wilcox Test (Does not assume normal distributions)
-    print(paste("Number Events:",nrow(record.events),",Number non-Events:",nrow(record.noevents)))
-    results <- wilcox.test(record.events$value,record.noevents$value,alternative)
-
-    #Box and whisker plot of the two comparison groups
-    record.events$Event <- 'Event'
-    record.noevents$Event <- 'No Event'
-    recordcompare <- rbind(record.events,record.noevents)
-    p <- ggplot(recordcompare)+
-      geom_boxplot(aes(x=as.factor(Event),y=value))+
-      ggtitle(paste("Comparison of Values for Climate Events: Overall"))+
-      ylab(expression(paste("Chl-a ",mu,"g/L")))+
-      xlab("")+
-      theme_bw()+
-      annotate("text",x="Event",y=-8,label=paste("Mean:",round(mean(record.events$value),2)))+
-      annotate("text",x="No Event",y=-8,label=paste("Mean:",round(mean(record.noevents$value),2)))
-    }
-  testtype <- function(alternative){
-    switch(alternative,
-           "two.sided" = "Difference in means is not equal to 0",
-           "greater" = "Difference in means is greater than 0. Mean for event is greater than mean for non-event.",
-           "less" = "Difference in means is less than 0. Mean for event is less than mean for non-event.")
-  }
-  if(!is.null(results)){
-    if(results$p.value>=0.05){
-      writeLines(paste("Not a statistically significant difference.\nP-value:",results$p.value))
-    }else{
-      writeLines(paste(testtype(alternative),"\nP-value:",results$p.value))
-    }
-  }
-  return(p)
-}
-
-#' climate.factor.location
-#'
-#' Performs a t-test on water quality dataset for various climate variables (by location)
+#' Evaluates difference in values based on climate conditions
 #'
 #' @param record dataframe with estimated historical record of water quality levels
 #' @param value string, name of column with estimated or field-sampled water quality parameter
 #' @param climaterecord dataframe with climate variables
 #' @param climatevar character, name of climate variable (column) of interest
-#' @param lag integer, number of days to lag the climate variable
 #' @param noevent numeric, threshold for whether an event occured
 #' @param alternative character string specifying alternative hypothesis ("two.sided","greater","less")
-#' @param location vector, list of unique location identifiers to iterate over
-#' @import stats
+#' @param overall boolean, TRUE: all locations, FALSE: by each location. Default is TRUE
+#' @param months months an optional character string for if the t-test should be month specific
+#' @param location string, name of column with unique location identifier, used if overall is FALSE
+#' @param ylabel string, optional label for plot
+#' @import ggplot2
 #' @import lubridate
 #' @export
 #'
 
+climate.factor.effect <- function(record,value,climaterecord,
+                                  climatevar,noevent,alternative="two.sided",overall=TRUE,months=NULL,location="",ylabel="Average Value"){
+  lag <- seq(0,7,1)
+  if(overall==TRUE){
+    if(!is.null(months)){
+      climaterecord$Month <- months(climaterecord$Date)
+      #Create results dataframe
+      results <- data.frame(Month=rep(months,8),Threshold=NA,Lag=rep(0:7,times=1,each=length(months)),Event=NA,NoEvent=NA,PValue=NA)
+      for(j in months){
+        if(noevent!=0){
+          climaterecordsub <- climaterecord[(climaterecord$Month==j),]
+          noevent <- mean(climaterecordsub[,climatevar],na.rm=TRUE)
+        }
+        for(i in lag){
+          climaterecord$templag <- lagpad(x=climaterecord[,climatevar],k=i)
+          climateeventdates <- climaterecord[(climaterecord$templag>noevent),'Date']
+          record.events <- subset(record,ImageDate %in% climateeventdates)
+          record.noevents <- subset(record,!(ImageDate %in% climateeventdates))
+          record.events$Month <- months(record.events$ImageDate)
+          record.noevents$Month <- months(record.noevents$ImageDate)
+          record.events.sub <- subset(record.events, Month %in% j)
+          record.noevents.sub <- subset(record.noevents, Month %in% j)
+          #Wilcox Test (Does not assume normal distributions)
+          wilcoxresults <- tryCatch(wilcox.test(record.events.sub[,value],record.noevents.sub[,value],alternative), error=function(e) NULL)
+          results[results$Lag==i & results$Month==j,"Threshold"] <- noevent
+          results[results$Lag==i & results$Month==j,"Event"] <- mean(record.events.sub[,value],na.rm=TRUE)
+          results[results$Lag==i & results$Month==j,"NoEvent"] <- mean(record.noevents.sub[,value],na.rm=TRUE)
+          if(!is.null(wilcoxresults)){
+            results[results$Lag==i & results$Month==j,"PValue"] <- wilcoxresults$p.value
+          }
 
-climate.factor.location <- function(record,value, climaterecord,climatevar,lag=NULL,noevent,alternative="two.sided",location){
-  record$value <- record[,value]
-  #If the variable is lagged:
-  if(!is.null(lag)){
-    #Lag climatevar
-    laggedvar <- lagpad(climaterecord[,climatevar],lag)
-    climaterecord[,paste(climatevar,'_lag')] <- laggedvar
-    #Subset into groups based on climate/weather events
-    climateeventdates <- climaterecord[laggedvar>noevent,'Date']
-    record.events <- subset(record,ImageDate %in% climateeventdates)
-    record.noevents <- subset(record,!(ImageDate %in% climateeventdates))
+
+        }
+      }
+    }else{
+      #Create results dataframe
+      results <- data.frame(Lag=seq(0,7,1),Event=NA,NoEvent=NA,PValue=NA)
+      for(i in lag){
+        climaterecord$templag <- lagpad(x=climaterecord[,climatevar],k=i)
+        climateeventdates <- climaterecord[(climaterecord$templag>noevent),'Date']
+        record.events <- subset(record,ImageDate %in% climateeventdates)
+        record.noevents <- subset(record,!(ImageDate %in% climateeventdates))
+        results[i+1,"Event"] <- mean(record.events[,value],na.rm=TRUE)
+        results[i+1,"NoEvent"] <- mean(record.noevents[,value],na.rm=TRUE)
+        results[i+1,"PValue"] <- wilcox.test(record.events[,value],record.noevents[,value],alternative)$p.value
+      }
+      #Plot Results
+      ggplot(data=results)+
+        geom_line(aes(x=Lag,y=Event,color='a'))+
+        geom_line(aes(x=Lag,y=NoEvent,color='b'))+
+        theme_bw()+
+        xlab("Lag (Days)")+
+        ylab(ylabel)+
+        scale_color_manual(name="",
+                           values=c('a'='red','b'='blue'),
+                           labels=c("Event","No Event"))
+    }
   }else{
-    #If the variable is not lagged:
-    #Subset into groups based on climate/weather events
-    climateeventdates <- climaterecord[(climaterecord[,climatevar]>noevent),'Date']
-    record.events <- subset(record,ImageDate %in% climateeventdates)
-    record.noevents <- subset(record,!(ImageDate %in% climateeventdates))
-  }
-  stationresults <- data.frame(Station=rep(NA,length(location)),PValue=rep(NA,length(location)))
-  if(nrow(record.noevents.sub)>0 & nrow(record.events.sub)>0){
-    recordcompare <- rbind(record.events.sub,record.noevents.sub)
-
-    for(i in seq(1,length(location))){
-      temp.data.event <- record.events[(record.events$StationID==location[i]),]
-      temp.data.noevent <- record.noevents[(record.noevents$StationID==location[i]),]
-      temp.results <- (wilcox.test(temp.data.event$value,temp.data.noevent$value,alternative="greater"))
-      stationresults$Station[i] <- as.character(location[i])
-      stationresults$PValue[i] <- temp.results$p.value
-      stationresults$Event[i] <- mean(temp.data.event$value)
-      stationresults$NoEvent[i] <- mean(temp.data.noevent$value)
+    #Get location info
+    locationlist <- unique(record[,location])
+    #Create Results dataframe
+    results <- data.frame(Location=rep(locationlist,8),Lag=rep(0:7,times=1,each=length(locationlist)),Event=NA,NoEvent=NA,PValue=NA)
+    for(j in locationlist){
+      recordsub <- record[(record[,location]==j),]
+      for(i in lag){
+        climaterecord$templag <- lagpad(x=climaterecord[,climatevar],k=i)
+        climateeventdates <- climaterecord[(climaterecord$templag>noevent),'Date']
+        record.events <- subset(recordsub,ImageDate %in% climateeventdates)
+        record.noevents <- subset(recordsub,!(ImageDate %in% climateeventdates))
+        results[(results$Lag==i & results$Location==j),"Event"] <- mean(record.events[,value],na.rm=TRUE)
+        results[(results$Lag==i & results$Location==j),"NoEvent"] <- mean(record.noevents[,value],na.rm=TRUE)
+        results[(results$Lag==i & results$Location==j),"PValue"] <- wilcox.test(record.events[,value],record.noevents[,value],alternative)$p.value
+      }
     }
   }
-
-
-  return(stationresults)
+  return(results)
 }
