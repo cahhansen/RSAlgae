f139e87f5e69c714fdd5f719dceedbfa9dfc429f
cahhansen <carlyahansen@gmail.com>
2018-02-28 19:28:05 -0700

    update sample scripts

--- R_Scripts/SampleFormatReflectanceData.R
+++ R_Scripts/SampleFormatReflectanceData.R
@@ -2,15 +2,16 @@
 setwd('~/GitHub/RSAlgae/RSAlgaeR/Data')
 calibrationdata <- read.csv('SampleLS5-7EarthEngineExport_Calibration.csv')
 
+#Format data used for model calibration-------------------------------------------------------------------
 
-#Specify Methods to be used (May vary by lake)
+#Specify Methods to be used (may vary by lake)
 calibrationdata <- calibrationdata[!(calibrationdata$Method=="Chlorophyll-A Uncorrected for pheophytin"),]
 #Format data used for model calibration
 formattedcalibrationdata <- formatSRdata(data=calibrationdata,imagerydate="ImageDate",samplingdate="SamplingDate",
                                          location="StationID",datatype="Calibration")
-write.csv(temp.df,file="SampleLS57CalibrationFormat.csv")
+write.csv(formattedcalibrationdata,file="SampleLS57CalibrationFormat.csv")
 
-#Format data used for model application
+#Format data used for model application-------------------------------------------------------------------
 historicdata <- read.csv('SampleLS5-7EarthEngineExport_Historic.csv')
 formattedhistoricdata <- formatSRdata(data=historicdata,imagerydate="ImageDate",
                                       location="StationID",datatype="Estimated")
