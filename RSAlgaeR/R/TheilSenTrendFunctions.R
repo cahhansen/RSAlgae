c0de9b0776627b1a46a5e078728de9cbcfaa99ab
cahhansen <carlyahansen@gmail.com>
2018-03-26 14:07:17 -0600

    fix annualtrend.ts function

--- RSAlgaeR/R/TheilSenTrendFunctions.R
+++ RSAlgaeR/R/TheilSenTrendFunctions.R
@@ -3,26 +3,31 @@
 #' Calculates annual linear trend of average values and significance of  with Theil-Sen Estimator (used for robust to non-normal data)
 #'
 #' @param record dataframe with record of estimated water quality
-#' @param value string, name of column with water quality values
-#' @param date string, name of column with dates
+#' @param valuecol string, name of column with water quality values
+#' @param datecol string, name of column with dates
 #' @param var string, aggregator (e.g. mean, max)
 #' @param monthlybias, calculates annual average using monthly averages (in case of differing numbers of samples for each month)
 #' @return summary of the Theil-Sen estimator
 #' @import lubridate
 #' @import mblm
 #' @import stats
+#' @examples
+#' data(estimatedrecord)
+#' annualtrend.ts(record=estimatedrecord,valuecol="EstChlValue",
+#' datecol="ImageDate",var="mean",monthlybias="TRUE")
 #' @export
 
 
-annualtrend.ts <- function(record,value,date,var,monthlybias=FALSE){
+annualtrend.ts <- function(record,valuecol,datecol,var,monthlybias=FALSE){
+  record$Date <- record[,datecol]
+  record$value <- record[,valuecol]
   if(monthlybias==TRUE){
-    record$year.norm <- lubridate::year(record[,date])-min(lubridate::year(record[,date]))
-    record$value <- record[[,value]]
+    record$year.norm <- lubridate::year(record$Date)-min(lubridate::year(record$Date))
+    record$Month <- lubridate::month(record$Date)
     yearlymonthlyrecord <- stats::aggregate(data=record,value~year.norm+Month, var, na.rm=TRUE)
     yearlyrecord <- stats::aggregate(data=yearlymonthlyrecord,value~year.norm,FUN=var)
   }else{
-    record$year.norm <- lubridate::year(record[,date])-min(lubridate::year(record[,date]))
-    record$value <- record[[,value]]
+    record$year.norm <- lubridate::year(record$Date)-min(lubridate::year(record$Date))
     yearlyrecord <- stats::aggregate(data=record,value~year.norm,FUN=var)
   }
   fit <- with(yearlyrecord,mblm::mblm(value~year.norm))
@@ -35,8 +40,8 @@ annualtrend.ts <- function(record,value,date,var,monthlybias=FALSE){
 #' Calculates annual linear trend of average monthly values and significance of  with Theil-Sen Estimator (used for robust to non-normal data)
 #'
 #' @param record dataframe with record of estimated water quality
-#' @param value string, name of column with values
-#' @param date string, name of column with dates
+#' @param valuecol string, name of column with values
+#' @param datecol string, name of column with dates
 #' @param months list of months
 #' @param var string, aggregator (e.g. mean, max)
 #' @return summary of the Theil-Sen estimator
@@ -45,12 +50,13 @@ annualtrend.ts <- function(record,value,date,var,monthlybias=FALSE){
 #' @import stats
 #' @export
 
-monthlytrend.ts <- function(record,value,date,months,var){
+monthlytrend.ts <- function(record,valuecol,datecol,months,var){
   monthlyresults <- data.frame(Month=months,Trend=rep(NA,length(months)),PValue=rep(NA,length(months)))
-  record$value <- record[[,value]]
+  record$value <- record[,valuecol]
+  record$Date <- record[,datecol]
   for(i in seq(1,length(months),1)){
     record.sub <- record[(record$Month==months[i]),]
-    record.sub$year.norm <- lubridate::year(record.sub[,date])-min(lubridate::year(record.sub[,date]))
+    record.sub$year.norm <- lubridate::year(record.sub$Date)-min(lubridate::year(record.sub$Date))
     yearlyrecord <- stats::aggregate(data=record.sub,value~year.norm,FUN=var)
     fit <- summary(with(yearlyrecord,mblm::mblm(value~year.norm)))
     monthlyresults[i,"Trend"] <- fit$coefficients[2,1]
